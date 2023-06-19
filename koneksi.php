23a9f273cdb748659e5ff02e67545ec5c8e853ec
Kadalgurun1337 <116977960+Kadalgurun1337@users.noreply.github.com>
2023-06-19 23:51:43 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,20 @@
+<?php
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+    }
+    function getConnection() {
+        $host = 'localhost';
+        $dbname = 'buku';
+        $username = 'root';
+        $password = '';
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+?>
